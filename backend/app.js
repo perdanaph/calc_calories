fdfce28e2cb92f822202079ed3066f361fec60ec
perdanaph <danangputro9@gmail.com>
2022-12-14 17:12:29 +0700

    feat: add route

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const router = require('./routers/index');
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  return res.status(200).json({ message: 'ok' });
-});
+// app.use(cors);
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(router);
 
-app.listen(3000, () => {
-  console.log(`Server running at http://localhost:3000`);
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
